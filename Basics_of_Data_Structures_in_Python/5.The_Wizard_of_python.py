c1d2cc1c2f0f67a8dcb64627ec9bbcef819810bb
Mahdie Dolatabadi <mahdiedolatabadiacademic@gmail.com>
2025-07-12 18:20:22 +0000

    5.The_Wizard_of_python.py

---
+++ Basics_of_Data_Structures_in_Python/5.The_Wizard_of_python.py
@@ -0,0 +1,10 @@
+# definition of sets
+SetA = {1, 2, 3, 4}
+SetB = {3, 4, 5, 6}
+
+# calculating union and intersection
+uni = SetA.union(SetB)
+inter = SetA.intersection(SetB)
+
+print(uni)
+print(inter)
