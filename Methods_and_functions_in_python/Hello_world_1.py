5f3ee22e32ce131666851e22d66817b9a3fdf05a
Mahdie Dolatabadi <mahdiedolatabadiacademic@gmail.com>
2025-07-29 23:37:49 +0000

    hello world 1 is completed

---
+++ Methods_and_functions_in_python/Hello_world_1.py
@@ -0,0 +1,8 @@
+def greet(name):
+    """Function to greet a person with their name."""
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    greet(name)
+    
